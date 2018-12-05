4a97da126e6c4aaa959ded4b557b9b271f1dba74
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-12-05 00:45:30 -0500

    Corrections to exercise 12 (Combine Datasets).

--- Lesson7/12quizCombineDatasets-reducer.py
+++ Lesson7/12quizCombineDatasets-reducer.py
@@ -12,20 +12,22 @@ def reducer():
     reader = csv.reader(sys.stdin, delimiter='\t')
     writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"',
                         quoting=csv.QUOTE_ALL)
-    output = [None, None, None, None,
-              None, None, None, None,
-              None, None, None, None, None]
+    #output = [None, None, None, None,
+    #          None, None, None, None,
+    #          None, None, None, None, None]
+    output = []
     for line in reader:
-        if 10 == line[1]:
-            output[9] = line[1]
-            output[10] = line[2]
-            output[11] = line[3]
-            output[12] = line[4]
-        elif 20 == line[1]:
-            output[0:9] = line[0:9]
+        if 6 == len(line):
+            output[9:13] = line[1:5]
+        elif 10 == len(line):
+            output[0:10] = line[0:10]
         else:
             continue
         writer.writerow(output)
+
+        if output and output[0] != line[0]:
+            output[0] = line[0]
+            continue
 
 
 if '__main__' == __name__:
